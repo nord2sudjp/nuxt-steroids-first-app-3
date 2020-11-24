da07ed0fd6332bcfcb29af715a1ce2e485fc35e3
maka y <nord2sudjp@gmail.com>
2020-11-24 21:30:21 +0900

    nuxtServerInit

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -11,36 +11,10 @@ export default {
   components: {
     PostList
   },
-  asyncData(context) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-       resolve({
-          loadedPosts: [
-            {
-              id: "1",
-              title: "First Post",
-              previewText: "This is our first post!",
-              thumbnail:
-                "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-            },
-            {
-              id: "2",
-              title: "Second Post",
-              previewText: "This is our second post!",
-              thumbnail:
-                "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-            }
-          ]
-        });
-      }, 1000);
-      // reject(new Error())
-    })
-    .then(data => {
-      return data
-    })
-    .catch(e => {
-      context.error(e);
-    });
+  computed: {
+    loadedPosts() {
+      return this.$store.getters.loadedPosts
+    }
   }
 };
 </script>
