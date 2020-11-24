150eee669f859696981e0e83b1963f22a7a70f68
maka y <nord2sudjp@gmail.com>
2020-11-24 20:36:34 +0900

    implement promise to handle error

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -11,27 +11,36 @@ export default {
   components: {
     PostList
   },
-  asyncData(context, callback) {
-    setTimeout(() => {
-      callback(null, {
-        loadedPosts: [
-          {
-            id: "1",
-            title: "First Post",
-            previewText: "This is our first post!",
-            thumbnail:
-              "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-          },
-          {
-            id: "2",
-            title: "Second Post",
-            previewText: "This is our second post!",
-            thumbnail:
-              "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-          }
-        ]
-      });
-    }, 1000);
+  asyncData(context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+       resolve({
+          loadedPosts: [
+            {
+              id: "1",
+              title: "First Post",
+              previewText: "This is our first post!",
+              thumbnail:
+                "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+            },
+            {
+              id: "2",
+              title: "Second Post",
+              previewText: "This is our second post!",
+              thumbnail:
+                "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+            }
+          ]
+        });
+      }, 1000);
+      // reject(new Error())
+    })
+    .then(data => {
+      return data
+    })
+    .catch(e => {
+      context.error(e);
+    });
   }
 };
 </script>
